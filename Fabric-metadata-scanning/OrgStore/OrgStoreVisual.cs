209f0110b3dd3c35520b319847b9feefa7fc7c17
Gennady Laventman <glaventman@microsoft.com>
2024-07-31 22:55:53 +0300

    Handling empty column header in CSV
    Signed-off-by: Gennady Laventman <glaventman@microsoft.com>

--- Fabric-metadata-scanning/OrgStore/OrgStoreVisual.cs
+++ Fabric-metadata-scanning/OrgStore/OrgStoreVisual.cs
@@ -1,9 +1,8 @@
-﻿namespace Fabric_metadata_scanning
+﻿using CsvHelper.Configuration;
+using Microsoft.PowerBI.Api.Models;
+
+namespace Fabric_metadata_scanning
 {
-
-
-
-
     public class OrgStoreVisual
     {
         public string guid { get; set; }
@@ -11,6 +10,17 @@
         public string name { get; set; }
         public string source { get; set; }
         public string changed { get; set; }
-        public string visualizationPane { get; set; }
     }
+    public class OrgStoreVisualMap : ClassMap<OrgStoreVisual>
+    {
+        public OrgStoreVisualMap()
+        {
+            Map(m => m.guid).Index(0); // First column
+            Map(m => m.name).Index(1); // Second column
+            Map(m => m.source).Index(2); // Empty header column
+            Map(m => m.changed).Index(3); // Fourth column
+            Map(m => m.disabled).Index(4); // Fourth column
+        }
+    }
+
 }
